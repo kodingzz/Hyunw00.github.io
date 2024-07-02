959c3b851d274fc7a0460fb6aed20d9529bb0a1d
HyunooTv <96560273+kodingzz@users.noreply.github.com>
2024-07-02 23:20:03 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@
  * 사용하는 기능들
  *  1. 웹사이트를 아래로 스크롤할때 header의 배경색 어둡게 변경
  *  2. home section을 지날때 home의 투명도를 점차적으로 늘린다.
- * 
+ *  3. arrow btn을 아래로 스크롤하면 사라지게 한다.
  */
-
-
 
 const header =document.querySelector('.header');
 const headerHeight =header.getBoundingClientRect().height;
@@ -17,11 +15,13 @@ const homeImg =document.querySelector('.home__img');
 const homeInfo = document.querySelector('.home__info');
 const homeHeight =home.offsetHeight;
 
+const arrowUp = document.querySelector('.arrowup');
 
 document.addEventListener('scroll',()=>{
     headerDarker();
     homeBlurred();
-}
+    showArrowBtn(); 
+}   
 )
 
 function headerDarker(){
@@ -37,5 +37,24 @@ function headerDarker(){
 function homeBlurred(){
     homeImg.style.opacity =1-window.scrollY/homeHeight; 
     homeInfo.style.opacity =1-window.scrollY/homeHeight; 
-    console.log(1-window.scrollY/homeHeight);
 }
+
+
+function showArrowBtn(){
+
+ 
+    if(window.scrollY>=homeHeight){
+        arrowUp.style.opacity = "1"; 
+    }
+    else{
+        arrowUp.style.opacity = "0";
+        /*
+         display:none, visibility로 대체가능하지만 이 속성들은
+         transition이 적용되지 않는다.
+         */
+    }
+}
+
+arrowUp.addEventListener('click',()=>{
+    window.scrollTo({top:0,behavior:"smooth"});
+})
